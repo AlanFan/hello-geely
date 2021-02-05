a9839c834503adb6c5b7d998f443760e7cb0e792
范培伦 <fanpeilun@163.com>
2021-02-05 15:36:31 +0800

    add hostname

--- src/main/java/com/geely/hellogeely/HelloGeelyApplication.java
+++ src/main/java/com/geely/hellogeely/HelloGeelyApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 @RestController
 public class HelloGeelyApplication {
 
 	@GetMapping("/")
-	public String hello(){
-		return "Hello Geely!";
+	public String hello() throws Exception{
+		return "Hello Geely!" + InetAddress.getLocalHost().getHostName();
 	}
 
 	public static void main(String[] args) {
