369bc74ef1a9acde1e726697e83752b6b8a049ef
范培伦 <fanpeilun@163.com>
2021-03-15 14:08:33 +0800

    test

--- src/main/java/com/geely/hellogeely/HelloGeelyApplication.java
+++ src/main/java/com/geely/hellogeely/HelloGeelyApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.VM;
 
 import java.net.InetAddress;
 
@@ -13,11 +14,11 @@ public class HelloGeelyApplication {
 
 	@GetMapping("/")
 	public String hello() throws Exception{
+		System.out.println(VM.maxDirectMemory());
 		return "Hello Geely!" + InetAddress.getLocalHost().getHostName();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloGeelyApplication.class, args);
 	}
-
 }
