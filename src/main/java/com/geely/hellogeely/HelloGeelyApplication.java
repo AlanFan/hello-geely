2b367f479bccb62649f2e476320e7786471a2016
范培伦 <fanpeilun@163.com>
2021-03-15 15:36:12 +0800

    testfff

--- src/main/java/com/geely/hellogeely/HelloGeelyApplication.java
+++ src/main/java/com/geely/hellogeely/HelloGeelyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import sun.misc.VM;
 
 import java.net.InetAddress;
+import java.nio.ByteBuffer;
 
 @SpringBootApplication
 @RestController
@@ -16,6 +17,12 @@ public class HelloGeelyApplication {
 	public String hello() throws Exception{
 		System.out.println(VM.maxDirectMemory());
 		return "Hello Geely!" + InetAddress.getLocalHost().getHostName();
+	}
+
+	@GetMapping("/test")
+	public String test(){
+		ByteBuffer bb = ByteBuffer.allocateDirect(1024*1024*512);
+		return "";
 	}
 
 	public static void main(String[] args) {
