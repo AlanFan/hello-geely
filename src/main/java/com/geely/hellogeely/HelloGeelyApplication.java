7d44dd64c1d0388b0b10c5e072dd97da2a60d602
范培伦 <fanpeilun@163.com>
2021-03-15 16:43:45 +0800

    ff

--- src/main/java/com/geely/hellogeely/HelloGeelyApplication.java
+++ src/main/java/com/geely/hellogeely/HelloGeelyApplication.java
@@ -3,25 +3,29 @@ package com.geely.hellogeely;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import sun.misc.VM;
 
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class HelloGeelyApplication {
-
+	List list  =  new ArrayList();
 	@GetMapping("/")
 	public String hello() throws Exception{
 		System.out.println(VM.maxDirectMemory());
 		return "Hello Geely!" + InetAddress.getLocalHost().getHostName();
 	}
 
-	@GetMapping("/test")
-	public String test(){
-		ByteBuffer bb = ByteBuffer.allocateDirect(1024*1024*512);
+	@GetMapping("/test/{size}")
+	public String test(@PathVariable int size){
+
+		list.add(ByteBuffer.allocateDirect(1024*1024*size));
 		return "";
 	}
 
